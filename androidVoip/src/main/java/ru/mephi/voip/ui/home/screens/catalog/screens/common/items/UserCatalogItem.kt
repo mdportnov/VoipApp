da73983a9e31e1eeb163b648191676f553a01878
TRANSAERO21 <martynenkodima21@gmail.com>
2022-08-24 14:46:26 +0300

    androidVoIP: ui: catalog: slightly improved user's card

--- androidVoip/src/main/java/ru/mephi/voip/ui/home/screens/catalog/screens/common/items/UserCatalogItem.kt
+++ androidVoip/src/main/java/ru/mephi/voip/ui/home/screens/catalog/screens/common/items/UserCatalogItem.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.buildAnnotatedString
@@ -25,6 +24,8 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import coil.request.CachePolicy
+import coil.request.ImageRequest
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.koin.androidx.compose.inject
@@ -78,11 +79,7 @@ internal fun UserCatalogItem(
                 }
             },
         ) {
-            Toast.makeText(
-                activity,
-                viewModel.addToFavourites(appointment).text,
-                Toast.LENGTH_SHORT
-            ).show()
+            Toast.makeText(activity, viewModel.addToFavourites(appointment).text, Toast.LENGTH_SHORT).show()
         }
     }
     Card(
@@ -100,56 +97,61 @@ internal fun UserCatalogItem(
     ) {
         Row(
             modifier = Modifier
-                .padding(bottom = 1.5.dp, top = 1.5.dp)
+                .padding(vertical = 6.dp, horizontal = 4.dp)
                 .fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically
         ) {
             AsyncImage(
                 modifier = Modifier
-                    .padding(start = 6.dp, end = 10.dp, top = 4.dp, bottom = 4.dp)
+                    .padding(start = 4.dp, end = 10.dp)
                     .size(42.dp)
                     .clip(CircleShape),
                 contentScale = ContentScale.Crop,
-                model = getImageUrl(appointment.line),
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(getImageUrl(appointment.lineShown))
+                    .memoryCachePolicy(CachePolicy.ENABLED)
+                    .diskCachePolicy(CachePolicy.ENABLED)
+                    .build(),
                 error = painterResource(id = R.drawable.ic_dummy_avatar),
                 placeholder = painterResource(id = R.drawable.ic_dummy_avatar),
                 contentDescription = null
             )
-            Column(
-                modifier = Modifier
-                    .width(
-                        (LocalConfiguration.current.screenWidthDp - 162).dp
-                    )
-                    .wrapContentHeight(),
-                horizontalAlignment = Alignment.Start,
-                verticalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(
-                    text = appointment.fio,
-                    style = MaterialTheme.typography.titleMedium,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis,
-                )
-                Text(
-                    text = buildAnnotatedString {
-                        withStyle(style = MaterialTheme.typography.labelLarge.toSpanStyle()) {
-                            append("Номер: ")
-                        }
-                        withStyle(style = MaterialTheme.typography.bodyMedium.toSpanStyle()) {
-                            append(appointment.line)
-                        }
-                    },
-                    maxLines = 1,
-                    overflow  = TextOverflow.Ellipsis
-                )
-            }
             Row(
-                modifier = Modifier.wrapContentSize()
+                modifier = Modifier.wrapContentHeight(),
+                verticalAlignment = Alignment.CenterVertically
             ) {
-                UserActionButton(Icons.Default.Star) {
-                    Toast.makeText(activity, viewModel.addToFavourites(appointment).text, Toast.LENGTH_SHORT).show()
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                        .wrapContentHeight(),
+                    horizontalAlignment = Alignment.Start,
+                    verticalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = appointment.fio,
+                        style = MaterialTheme.typography.titleMedium,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                    )
+                    Text(
+                        text = if (appointment.lineShown.isNotEmpty()) {
+                            "Номер: ${appointment.lineShown}"
+                        } else {
+                            "Номер отсуствует"
+                        },
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        style = MaterialTheme.typography.labelLarge
+                    )
                 }
-                UserActionButton(Icons.Default.Call) { onCallClick(appointment.line) }
+                Row(
+                    modifier = Modifier.wrapContentSize()
+                ) {
+                    UserActionButton(Icons.Default.Star) {
+                        Toast.makeText(activity, viewModel.addToFavourites(appointment).text, Toast.LENGTH_SHORT).show()
+                    }
+                    UserActionButton(Icons.Default.Call) { onCallClick(appointment.line) }
+                }
             }
         }
     }
